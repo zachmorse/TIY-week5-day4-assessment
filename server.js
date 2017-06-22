1b6f662f95d572e914bc853e47b73dfec59d107f
Zach Morse <zacharymorse@gmail.com>
2017-06-22 17:25:32 -0500

    working version

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const sessionConfig = require("./sessionConfig");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
-var users = [];
-
+var users = [
+  { username: "Zach", password: "elcamin0" },
+  { username: "bill", password: "5678" }
+];
+var currentUser = [];
 // set view engine:
 
 app.engine("mustache", mustacheExpress());
@@ -19,3 +22,54 @@ app.set("view engine", "mustache");
 app.use("/", express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
+
+function checkAuth(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  } else {
+    next();
+  }
+}
+
+// ROUTES
+
+app.get("/", checkAuth, function(req, res) {
+  res.render("index", { userListing: currentUser });
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.post("/login", function(req, res) {
+  if (!req.body || !req.body.username || !req.body.password) {
+    return res.redirect("/login");
+  }
+
+  var requestingUser = req.body;
+  var userRecord;
+  users.forEach(function(item) {
+    if (item.username === requestingUser.username) {
+      userRecord = item;
+      currentUser = item.username;
+    }
+  });
+
+  if (!userRecord) {
+    return res.redirect("/login");
+  }
+
+  if (requestingUser.password === userRecord.password) {
+    req.session.user = userRecord;
+
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
+});
+
+// LISTENER
+
+app.listen(port, function(req, res) {
+  console.log("Server running on port", port);
+});
